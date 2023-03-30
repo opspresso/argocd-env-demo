9e2a897f83c5356055588915985a5a379f3174ca
nalbam <me@nalbam.com>
2023-03-30 16:11:33 +0900

    update_versions

--- helm.py
+++ helm.py
@@ -3,7 +3,9 @@
 
 import argparse
 import base64
+import datetime
 import hashlib
+import json
 import os
 import yaml
 
@@ -22,17 +24,54 @@ def parse_args():
     p.add_argument("-n", "--imagename", default=IMAGENAME, help="imagename")
     p.add_argument("-v", "--version", default=VERSION, help="version")
     p.add_argument("-c", "--container", default="", help="container")
+    p.add_argument("-c", "--approved", default=False, help="approved")
     return p.parse_args()
+
+
+def update_versions(args):
+    filepath = "charts/{}/versions-{}.json".format(args.reponame, args.phase)
+
+    docs = None
+
+    current_time = datetime.datetime.now()
+
+    if os.path.exists(filepath):
+        print("update_versions", filepath)
+
+        with open(filepath, "r") as file:
+            docs = json.load(file)
+
+    else:
+        docs = {}
+        docs["version"] = ""
+        docs["items"] = []
+
+    if args.approved == True:
+        docs["version"] = args.version
+        docs["approved"] = current_time.isoformat()
+
+    isExists = False
+    for i, doc in enumerate(docs["items"]):
+        if doc["version"] == args.version:
+            isExists = True
+            break
+
+    if isExists == False:
+        docs["items"].append({"version": args.version, "updated": current_time.isoformat()})
+
+    if docs != None:
+        with open(filepath, "w") as file:
+            json.dump(docs, file, sort_keys=True, indent=2)
 
 
 def replace_values(args):
     filepath = "charts/{}/values-{}.yaml".format(args.reponame, args.phase)
     filehash = ""
 
+    docs = None
+
     if os.path.exists(filepath):
         print("replace_values", filepath)
-
-        docs = None
 
         with open(filepath, "r") as file:
             docs = yaml.safe_load(file)
@@ -53,16 +92,14 @@ def replace_values(args):
 
                 # secret
                 if "secret" in docs[doc]:
-                    docs[doc]["secret"]["data"]["SECRET_VERSION"] = base64.b64encode(
-                        args.version.encode("utf-8")
-                    )
+                    docs[doc]["secret"]["data"]["SECRET_VERSION"] = base64.b64encode(args.version.encode("utf-8"))
 
-        if docs != None:
-            with open(filepath, "w") as file:
-                yaml.safe_dump(docs, file)
+    if docs != None:
+        with open(filepath, "w") as file:
+            yaml.safe_dump(docs, file)
 
-            with open(filepath, "rb") as file:
-                filehash = hashlib.md5(file.read()).hexdigest()
+        with open(filepath, "rb") as file:
+            filehash = hashlib.md5(file.read()).hexdigest()
 
     return filehash
 
@@ -71,10 +108,10 @@ def replace_hash(args, hash):
     filepath = "charts/{}/values-{}.yaml".format(args.reponame, args.phase)
     filehash = ""
 
+    docs = None
+
     if os.path.exists(filepath):
         print("replace_hash", filepath)
-
-        docs = None
 
         with open(filepath, "r") as file:
             docs = yaml.safe_load(file)
@@ -88,18 +125,20 @@ def replace_hash(args, hash):
                         if env["name"] == "ENV_HASH":
                             env["value"] = hash
 
-        if docs != None:
-            with open(filepath, "w") as file:
-                yaml.safe_dump(docs, file)
+    if docs != None:
+        with open(filepath, "w") as file:
+            yaml.safe_dump(docs, file)
 
-            with open(filepath, "rb") as file:
-                filehash = hashlib.md5(file.read()).hexdigest()
+        with open(filepath, "rb") as file:
+            filehash = hashlib.md5(file.read()).hexdigest()
 
     return filehash
 
 
 def main():
     args = parse_args()
+
+    update_versions(args)
 
     hash = replace_values(args)
 
