6c4b91b745dc1052a5dc4ea1e66cebcb7a93633d
nalbam <me@nalbam.com>
2024-01-20 12:47:22 +0900

    gitops

--- hpa/hpa.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-k get hpa -A -o json | jq '.items[] | {NAME:.metadata.name,UTIL:.spec.targetCPUUtilizationPercentage, MIN:.spec.minReplicas, MAX:.spec.maxReplicas, CURRENT:.status.currentReplicas}'
